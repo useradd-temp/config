b97fd3f9b57438225815b50de36e591dec9f2d66
SeungHak Lee <useradd.temp@gmail.com>
2025-02-13 14:56:54 +0900

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,18 +1,39 @@
 if status is-interactive
-    eval /Users/seunghaklee/miniforge3/bin/conda "shell.fish" "hook" $argv | source
+    # Commands to run in interactive sessions can go here
 end
 
-function starship_transient_prompt_func
-  starship module character
-end
+set -x PAGER cat
 starship init fish | source
-enable_transience
 
-
-alias k=kubectl
+eval "$(/opt/homebrew/bin/brew shellenv)"
+alias dev="cd ~/Dev"
 alias tf=terraform
 
-export GOPATH=$GOROOT/gopath
-export GOBIN=$GOPATH/bin
-set -gx PATH $PATH $GOROOT/bin
-set -gx PATH $PATH $GOBIN
+
+function kubectl --wraps kubectl
+  env KUBECOLOR_CONFIG="$HOME/.config/kubecolor.yaml" command kubecolor $argv
+end
+
+function k --wraps kubectl
+  env KUBECOLOR_CONFIG="$HOME/.config/kubecolor.yaml" command kubecolor $argv
+end
+
+
+function kw --wraps kubectl
+  env KUBECOLOR_FORCE_COLOR=1  KUBECOLOR_CONFIG="$HOME/.config/kubecolor-watch.yaml" command watch -n 1 -c unbuffer kubecolor $argv
+end
+
+function kubecolor --wraps kubectl
+  command kubecolor $argv
+end
+
+kubectl completion fish | source
+
+set -gx  PATH $PATH $GOPATH/bin
+set -gx  PATH $PATH $HOME/.krew/bin
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+#
+#
+#
